7f52d2d73e8499a6300e8f31080a7eba816fb26d
Dascesa <francir242@gmail.com>
2024-05-02 10:49:54 -0300

    comando iniciar aula, salva usuario em banco, tabela de produtos

--- resources/views/cria-conta.blade.php
+++ resources/views/cria-conta.blade.php
@@ -1,24 +1,17 @@
 <x-layout_base>
+
+<!-- Tentando colocar um fundo de tela -->
+<div class="bg-repeat ..." style="background-image: url(...)"></div>
+<!-- Finalzinho do fundo -->
+
 <!-- Tela do criar conta -->
-
-<style>
-  .teste{
-background-color: #DFDBE5;
-background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 10h10v10H0V10zM10 0h10v10H10V0z' fill='%230ccdfb' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
-  }
-</style>
-
-<label class="flex cursor-pointer gap-2 ">
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
-  <input type="checkbox" value="synthwave" class="toggle theme-controller"/>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-</label>
 
 <!-- Menu -->
 <div class="max-w-md m-auto">
 
 <form class="teste" action="{{route('salva-usuario')}}" method="POST">
   @csrf
+  
 <!-- Usuario -->
 <div class="divider divider-accent"></div>
 <label class="input input-bordered flex items-center gap-2 justify-center flex">
@@ -51,9 +44,6 @@ background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox=
 
 <!-- Botão -->
 <button type="submit" class="btn btn-primary">Criar conta</button>
-
-<!-- Layout de fundo da tela Marca dágua -->
-
 
 </form>
 <!-- Menu de cima OK -->
